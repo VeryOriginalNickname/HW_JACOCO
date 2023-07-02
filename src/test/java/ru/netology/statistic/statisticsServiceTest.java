fd03b3cb224079ff5219504b165a533b1ed80747
Dmitry Kurganov <dimacup30@gmail.com>
2023-07-03 01:18:16 +0300

    Jacoco

--- src/test/java/ru/netology/statistic/statisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/statisticsServiceTest.java
@@ -10,7 +10,7 @@ public class statisticsServiceTest {
     void findMax() {
         statisticsService service = new statisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
